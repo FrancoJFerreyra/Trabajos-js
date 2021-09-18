ba37be48ae80213215713c11ab4db794c69ae5b9
Franco Ferreyra <ferreyra_franco02@outlook.com>
2021-09-18 09:39:14 -0300

    entrega clase 5

--- Js/main.js
+++ Js/main.js
@@ -1,20 +1,17 @@
-const precioTotal= (a,b,c) => {return a+b+c};
-const resta= (a,b) => {return a-b};
-const descuentoEfectivo= x => {return x * 0.10};
+ class producto{
+     constructor(nombre,precio){
+     this.nombre=nombre.toUppercase ;
+     this.precio=parseFloat(precio) ;
+ }
+ 
+ descuento(){
+     this.precio = this.precio * 0.10;
+ }
+ }
+ const arroz = new producto ("Arroz", "55");
+ const pollo = new producto ("pollo","400");
+ const salsa = new producto ("salsa","120");
 
-let arroz= 55;
-let pollo= 400;
-let salsa= 120;
-
-let precioTotalEfectivo =resta(precioTotal(arroz,pollo,salsa), descuentoEfectivo( precioTotal(arroz,pollo,salsa)))
-console.log(precioTotalEfectivo)
-
-// // Complementario
-// const dues = (a,b) => a / b;
-
-// let productPrice = prompt( "Ingrese el precio del producto");
-// let amountFees = prompt("Ingrese la cantidad de cuotas");
-
-// let totalPrice = dues (productPrice, amountFees);
-
-// console.log (totalPrice);
+ arroz.descuento();
+ pollo.descuento();
+ salsa.descuento();
