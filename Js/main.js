e10f8adb5e9e7d3be2c844b2768250a9826264ba
Franco Ferreyra <ferreyra_franco02@outlook.com>
2021-09-19 15:20:40 -0300

    entrega clase 6 obligatorio

--- Js/main.js
+++ Js/main.js
@@ -1,17 +1,25 @@
- class producto{
-     constructor(nombre,precio){
-     this.nombre=nombre.toUppercase ;
-     this.precio=parseFloat(precio) ;
- }
- 
- descuento(){
-     this.precio = this.precio * 0.10;
- }
- }
- const arroz = new producto ("Arroz", "55");
- const pollo = new producto ("pollo","400");
- const salsa = new producto ("salsa","120");
+const productos= [
+    {id: 1, producto: "zapatillas nike1", marca: "nike", precio: 4400},
+    {id: 2, producto: "zapatillas nike2", marca: "nike", precio: 4850},
+    {id: 3, producto: "zapatillas adidas", marca:"adidas", precio: 4000},
+    {id: 4, producto: "zapatillas converse", marca:"converse", precio: 2800},
+    {id: 5, producto: "zapatillas rebook", marca: "rebook", precio: 2500}];
+const zapatillasNike = productos.find(producto=>producto.marca==="nike");
+console.log(zapatillasNike)
 
- arroz.descuento();
- pollo.descuento();
- salsa.descuento();
+const menorPrecio= productos.filter(producto=>producto.precio < 3000)
+console.log(menorPrecio)
+
+const iva= productos.map(producto=>producto.precio * 1.21)
+console.log(iva);
+
+productos.sort((a,b)=>{
+    if(a<b){
+    return -1
+    }
+    else(a>b)
+    {return 1;}
+    
+    return 0;
+})
+console.log(productos)
