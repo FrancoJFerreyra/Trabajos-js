3d93286dca06f7e2673c7065e06352c59588defb
Franco Ferreyra <ferreyra_franco02@outlook.com>
2021-10-09 12:57:28 -0300

    agregue un archivo data.js, agregue y modifique con css un button en cada card de productos.html

--- Js/main.js
+++ Js/main.js
@@ -1,89 +1,89 @@
-let nombre= prompt("Por favor ingrese su nombre");
-alert ("Buen dia" + " " + nombre.toUpperCase());
+// let nombre= prompt("Por favor ingrese su nombre");
+// alert ("Buen dia" + " " + nombre.toUpperCase());
 
-const listaMarcas= ["1-Nike ","2-New Balance ", "3-Adidas ", "4-Converse ","5-Fila ", "6-Puma ", "7-Salir"]
-alert("A continuación verá la lista de marcas:" + " " + listaMarcas.toString())
+// const listaMarcas= ["1-Nike ","2-New Balance ", "3-Adidas ", "4-Converse ","5-Fila ", "6-Puma ", "7-Salir"]
+// alert("A continuación verá la lista de marcas:" + " " + listaMarcas.toString())
 
 
-let filtrarMarca = prompt("Ingrese la marca de zapatillas");
-console.log (filtrarMarca.toUpperCase());
+// let filtrarMarca = prompt("Ingrese la marca de zapatillas");
+// console.log (filtrarMarca.toUpperCase());
 
-switchCarrito:
-while (filtrarMarca != 7){
-    switch(filtrarMarca){
-        case "1":
-            let opcionNike= prompt(modelo + " " + nikeAir+ "\n" + salir)
-        if(opcionNike==="1"){
-        alert(precioZapatilla + " " + "$"+zapatillasNike.precio)
-            }
-            else if(opcionNike==="2")break switchCarrito;
-        else alert(error);
-    break;
+// switchCarrito:
+// while (filtrarMarca != 7){
+//     switch(filtrarMarca){
+//         case "1":
+//             let opcionNike= prompt(modelo + " " + nikeAir+ "\n" + salir)
+//         if(opcionNike==="1"){
+//         alert(precioZapatilla + " " + "$"+zapatillasNike.precio)
+//             }
+//             else if(opcionNike==="2")break switchCarrito;
+//         else alert(error);
+//     break;
    
-    case "2":
-        let opcionAdidas= prompt(modelo + " " + adidasG+ "\n" + salir  );
-    if(opcionAdidas== "1"){
-    alert(precioZapatilla + " " + "$"+zapatillasAdidas.precio)
-            }
-        else if(opcionAdidas==="2") break switchCarrito;
-    else alert(error);   
+//     case "2":
+//         let opcionAdidas= prompt(modelo + " " + adidasG+ "\n" + salir  );
+//     if(opcionAdidas== "1"){
+//     alert(precioZapatilla + " " + "$"+zapatillasAdidas.precio)
+//             }
+//         else if(opcionAdidas==="2") break switchCarrito;
+//     else alert(error);   
 
-    break;
+//     break;
 
-    case "3":
-        let opcionNewBalance= prompt(modelo + " " + NBPink+ "\n" + salir  );
+//     case "3":
+//         let opcionNewBalance= prompt(modelo + " " + NBPink+ "\n" + salir  );
 
-    if(opcionNewBalance== "1"){   
-        alert(precioZapatilla + " " + "$"+zapatillasNB.precio)
-            }
-            else if(opcionNewBalance==="2") break switchCarrito;
-        else alert(error);   
-    break;
+//     if(opcionNewBalance== "1"){   
+//         alert(precioZapatilla + " " + "$"+zapatillasNB.precio)
+//             }
+//             else if(opcionNewBalance==="2") break switchCarrito;
+//         else alert(error);   
+//     break;
 
-    case "4":
-        let opcionConverse= prompt(modelo + " " + converse+ "\n" + salir  );
+//     case "4":
+//         let opcionConverse= prompt(modelo + " " + converse+ "\n" + salir  );
 
-    if(opcionConverse=="1"){
-    alert(precioZapatilla + " " + "$"+zapatillasConverse.precio)
-            }
-            else if(opcionConverse==="2") break switchCarrito;
-        else alert(error);   
-    break;
+//     if(opcionConverse=="1"){
+//     alert(precioZapatilla + " " + "$"+zapatillasConverse.precio)
+//             }
+//             else if(opcionConverse==="2") break switchCarrito;
+//         else alert(error);   
+//     break;
 
-    case "5":
-        let opcionFila= prompt(modelo + " " + fila+ "\n" + salir  );
+//     case "5":
+//         let opcionFila= prompt(modelo + " " + fila+ "\n" + salir  );
 
-    if(opcionFila== "1"){
-    alert(precioZapatilla + " " + "$"+zapatillasFila.precio)
-            }
-            else if(opcionFila==="2") break switchCarrito;
-        else alert(error);   
+//     if(opcionFila== "1"){
+//     alert(precioZapatilla + " " + "$"+zapatillasFila.precio)
+//             }
+//             else if(opcionFila==="2") break switchCarrito;
+//         else alert(error);   
 
-    break;
+//     break;
 
-    case "6":
-        let opcionPuma= prompt(modelo + " " + puma+ "\n" + salir  );
+//     case "6":
+//         let opcionPuma= prompt(modelo + " " + puma+ "\n" + salir  );
 
-    if (opcionPuma== "1"){
-    alert(precioZapatilla + " " + "$"+zapatillasPuma.precio)
-             }
-            else if(opcionPuma==="2") break switchCarrito;
-        else alert(error);   
-    break;
+//     if (opcionPuma== "1"){
+//     alert(precioZapatilla + " " + "$"+zapatillasPuma.precio)
+//              }
+//             else if(opcionPuma==="2") break switchCarrito;
+//         else alert(error);   
+//     break;
 
-    default: alert(error);
-    }
-}
+//     default: alert(error);
+//     }
+// }
 
-boton.onclick = () =>{
-for(const producto of productos ){
-    let listaProductos = document.createElement(`div`);
-    listaProductos.classList.add("div__products--cart");
-    listaProductos.innerHTML=
-        `<ul class="ul__products--cart">
-            <li>
-                 ID = ${producto.id}, Marca = ${producto.marca}, Modelo = ${producto.producto} Precio = $${producto.precio}
-             </li>
-         </ul>`
-     document.body.appendChild(listaProductos);
- }}
+// boton.onclick = () =>{
+// for(const producto of productos ){
+//     let listaProductos = document.createElement(`div`);
+//     listaProductos.classList.add("div__products--cart");
+//     listaProductos.innerHTML=
+//         `<ul class="ul__products--cart">
+//             <li>
+//                  ID = ${producto.id}, Marca = ${producto.marca}, Modelo = ${producto.producto} Precio = $${producto.precio}
+//              </li>
+//          </ul>`
+//      document.body.appendChild(listaProductos);
+//  }}
--- Js/variables.js
+++ Js/variables.js
@@ -1,19 +1,3 @@
-let modelo = "Seleccione un modelo de zapatilla entre los disponibles"
-const nikeAir= "\n1-Air Jordan" ;
-const adidasG="\n 1-Adidas Gazelle";
-const NBPink="\n 1-New Balance 530 Pink";
-const converse="\n 1-Converse all star";
-const fila= "\n 1-Fila Disruptor";
-const puma= "\n 1-Classic Puma Pink";
-
-const productos= [{id: 1, producto: "Air Jordan High 1 Panda", marca: "NIKE", precio: 4400},
-{id: 2, producto: "New Balance 530 Pink", marca: "NEW BALANCE", precio: 4850},
-{id: 3, producto: "Adidas Gazelle Unisex", marca:"ADIDAS", precio: 4000},
-{id: 4, producto: "Converse All Star Blue", marca:"CONVERSE", precio: 2800},
-{id: 5, producto: "Fila Disruptor", marca: "FILA", precio: 2500},
-{id: 6, producto: "Classic Puma Pink", marca: "PUMA", precio: 3500}
-];  
-
 const zapatillasNike = productos.find(producto=>producto.marca==="NIKE");
 const zapatillasAdidas = productos.find(producto=>producto.marca==="ADIDAS");
 const zapatillasNB = productos.find(producto=>producto.marca==="NEW BALANCE");
