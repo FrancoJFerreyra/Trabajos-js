2ca01e99bdcaed4ae6b7396d8e6805016db69b47
Franco Ferreyra <ferreyra_franco02@outlook.com>
2021-10-18 23:18:58 -0300

    utilice slide up y slide down para el carrito con jquery, entrega clase  13

--- Js/main.js
+++ Js/main.js
@@ -1,14 +1,16 @@
 createCards();
 
-// $("#cart").prepend(`<div id="cartWindow"></div>`);
+$("#cart").prepend(`<div id="cartWindow"></div>`);
 
 // $("#cart").click(()=>{
 //     let cartContentShow = localStorage.getItem("cart");
 //     cartContentShow= JSON.parse (cartContentShow);
 //    if(!cartContentShow) {
 //         alert("El carrito esta vacio!")
-// }else{
-//    $("#cartWindow").show()}
+// }else{}
 // })
-
-// $("#cartWindow").css("background-color", "pink")
+$("#cart").click(()=>{
+    $("#cartWindow").css("background-color", "pink")
+    .slideUp(1500)
+    .slideDown(1500);
+})
