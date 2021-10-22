5f5405cd4a247de8c631249b41a4d6911db76a8a
Franco Ferreyra <ferreyra_franco02@outlook.com>
2021-10-22 20:24:31 -0300

    elimine archivo variables y funciones.js

--- Js/funciones.js
+++
@@ -1,8 +0,0 @@
-const URLGET = `http://hp-api.herokuapp.com/api/characters`;
-$("#cart").click(()=>
-$.get(URLGET,function(respuesta,estado){
-    console.log(respuesta[0])
-    $("#cart").append(
-        `<div><img src="${respuesta[0].image}"</img></div>`
-    )
-}))
